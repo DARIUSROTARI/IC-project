568c91736d56e88144aa5dc7ccae934c6b0cd8dc
larisa <larisaaapele@yahoo.com>
2024-05-28 00:39:41 +0300

    Change UserLoginResponseDTO

--- backend/ArtDuParfum/src/main/java/artduparfum/ArtDuParfum/repository/dto/response/UserLoginResponseDTO.java
+++ backend/ArtDuParfum/src/main/java/artduparfum/ArtDuParfum/repository/dto/response/UserLoginResponseDTO.java
@@ -4,6 +4,9 @@ import lombok.Data;
 
 @Data
 public class UserLoginResponseDTO {
+    private String lastName;
+    private String firstName;
+    private String dateOfBirth;
     private String email;
     private Long id;
 }
